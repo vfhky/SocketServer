9d3eaf12ddb148b76c71db2e27251ae20213e787
ZL <luckylucky3210@163.com>
2015-07-04 23:35:43 +0800

    modify Makefile

--- inc/main.h
+++ inc/main.h
@@ -1,1 +1,7 @@
+#ifndef MAIN_H
 #define MAIN_H
+ 
+#define PORT 0
+#define CHILD_SVR_NUM 5
+ 
+#endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
+#include "main.h"
 #include "server.h"
 
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 	Server *svr = (Server*) malloc(sizeof(Server));
-	server_initialization(svr, NULL, 0);
-	server_create_tcpsvr(svr);
-    server_create_child_svr(svr, 5);
+	server_initialization(svr, NULL, PORT);
+	server_create_tcpsvr(svr);  
+    server_create_child_svr(svr, CHILD_SVR_NUM);
 	free(svr);
 	exit(EXIT_SUCCESS);
 }
